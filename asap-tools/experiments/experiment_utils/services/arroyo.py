a881811bfb137eb7185afccf098906e9b9b255ad
Gnanesh <gnanesh.dometti@gmail.com>
2026-03-20 15:30:18 -0400

    Simplify UDF impl mode, default all to sketchlib

--- asap-tools/experiments/experiment_utils/services/arroyo.py
+++ asap-tools/experiments/experiment_utils/services/arroyo.py
@@ -105,9 +105,9 @@ class ArroyoService(BaseService):
         use_kafka_ingest: bool = False,
         enable_optimized_remote_write: bool = False,
         avoid_long_ssh: bool = False,
-        sketch_cms_impl: str = "legacy",
-        sketch_kll_impl: str = "legacy",
-        sketch_cmwh_impl: str = "legacy",
+        sketch_cms_impl: str = "sketchlib",
+        sketch_kll_impl: str = "sketchlib",
+        sketch_cmwh_impl: str = "sketchlib",
     ) -> str:
         """
         Run ArroyoSketch pipeline.
